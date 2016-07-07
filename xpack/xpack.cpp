d35d059a7d4ea7f920772f6313ccf77e867fc567
lincolnfz <lincolnfz@gmail.com>
2016-07-07 10:36:14 +0800

    code review

--- xpack/xpack.cpp
+++ xpack/xpack.cpp
@@ -8,16 +8,16 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	zipFile2PackFile( "e:\\webui" , "e:\\ui.pack");
+	//zipFile2PackFile( "e:\\webui" , "e:\\ui.pack");
 
 	unsigned char *buf = NULL;
 	DWORD dwlen = 0;
-	bool ext = exZipFile( "e:\\ui.pack" , "\\demo\\main\\Index.shtml" , &buf , &dwlen );
+	bool ext = exZipFile( L"D:\\Ê¿´ó·ò (2)\\ui.pack" , L"\\Index.html" , &buf , &dwlen );
 	//exZipFile( "D:\\ui.pack" , "\\bookmark.htm" , &buf , &dwlen );
 
 	if ( buf )
 	{
-		FILE *fp = fopen("e:\\test.html" , "wb");
+		FILE *fp = fopen("d:\\test.html" , "wb");
 		fwrite( buf , 1 , dwlen , fp );
 		fclose(fp);
 		freeBuf(buf);
