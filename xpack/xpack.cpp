025f44cd718e906a0bd90d977d4a44efdb88dbb3
lincolnfz <lincolnfz@gmail.com>
2016-09-22 11:44:17 +0800

    demo

--- xpack/xpack.cpp
+++ xpack/xpack.cpp
@@ -8,16 +8,16 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//zipFile2PackFile( "e:\\webui" , "e:\\ui.pack");
+	zipFile2PackFile( L"d:\\test士大121夫  (231)" , L"d:\\界面1.pack");
 
 	unsigned char *buf = NULL;
 	DWORD dwlen = 0;
-	bool ext = exZipFile( L"D:\\士大夫 (2)\\ui.pack" , L"\\Index.html" , &buf , &dwlen );
+	bool ext = exZipFile( L"D:\\士大夫 (2)\\界面1.pack" , L"\\图标1.png" , &buf , &dwlen );
 	//exZipFile( "D:\\ui.pack" , "\\bookmark.htm" , &buf , &dwlen );
 
 	if ( buf )
 	{
-		FILE *fp = fopen("d:\\test.html" , "wb");
+		FILE *fp = fopen("d:\\test.png" , "wb");
 		fwrite( buf , 1 , dwlen , fp );
 		fclose(fp);
 		freeBuf(buf);
